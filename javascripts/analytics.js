ae4b90bdac5241116f4b92193ee37b9e8d0e5a36
Félix Saparelli <me@passcod.net>
2012-07-03 20:35:47 +1200

    Add gauges track

---
+++ javascripts/analytics.js
@@ -0,0 +1,11 @@
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '4ff29adc613f5d67b8000030');
+    t.src = '//secure.gaug.es/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
